880de3f820d2a0ab923945d7e7e4d6f9aa52159a
MARIJA-LT\Marija <mmilosevic1411@gmail.com>
2020-09-24 14:13:59 +0200

    no message

--- praksa/src/components/VehicleData.jsx
+++ praksa/src/components/VehicleData.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const VehicleData = [
   {
     id: 1,
--- praksa/src/components/VehicleList.js
+++ praksa/src/components/VehicleList.js
@@ -1,20 +1,41 @@
 import React from "react";
 
-function VehicleList(props) {
-  const items = props.items;
-  const listItems = items.map((item) => {
-    return (
-      <div className="list" key={item.key}>
-        <p>
-          {item.text}
-          <span>
-            <button onClick={() => props.deleteItem(item.key)}>Delete</button>
-          </span>
-        </p>
-      </div>
-    );
-  });
-  return <div>{listItems}</div>;
+class VehicleList extends React.Component {
+  render() {
+    const items = this.props.items;
+    const listItems = items.map((item) => {
+      return (
+        <div className="list" key={item.key}>
+          <p>
+            {item.text}
+            <span>
+              <button onClick={() => this.props.deleteItem(item.key)}>
+                Delete
+              </button>
+            </span>
+          </p>
+        </div>
+      );
+    });
+    return <div>{listItems}</div>;
+  }
 }
+
+// function VehicleList(props) {
+//   const items = props.items;
+//   const listItems = items.map((item) => {
+//     return (
+//       <div className="list" key={item.key}>
+//         <p>
+//           {item.text}
+//           <span>
+//             <button onClick={() => props.deleteItem(item.key)}>Delete</button>
+//           </span>
+//         </p>
+//       </div>
+//     );
+//   });
+//   return <div>{listItems}</div>;
+// }
 
 export default VehicleList;
